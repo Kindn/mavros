fe73c676794e986ba12a58b7cb4e54a3bc4714df
Kindn <1434615509@qq.com>
2026-06-28 16:55:13 +0800

    Add IMU data correction based on calib info

---
+++ mavros/include/simple_json/exceptions.hpp
@@ -0,0 +1,27 @@
+#ifndef _EXCEPTIONS_HPP_
+#define _EXCEPTIONS__HPP_
+
+#include <exception>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+namespace sjson {
+class JsonError : public std::exception {
+private:
+  std::stringstream msg;
+
+public:
+  JsonError(const std::string _description, int _line, int _col,
+            bool _print_pos = true) {
+    msg << "[JsonError]";
+    msg << _description;
+    if (_print_pos)
+      msg << "(line " << _line << " col " << _col << "\n";
+  }
+
+  const char *what() const throw() { return msg.str().c_str(); }
+};
+} // namespace sjson
+
+#endif //_EXCEPTIONS__HPP_
